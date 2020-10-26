d78714bb7e26ea5275e991259051a306a108fba4
Widikin <widikin@Julies-MacBook-Pro.local>
2020-10-26 12:43:42 +0200

    finished learning about functions and variables

---
+++ functions/python.py
@@ -0,0 +1,27 @@
+import turtle
+
+a = 5
+print(a)
+
+b = "hello"
+print(b)
+
+credit_card = 44443333222211111
+
+print(credit_card)
+
+# Square
+qazi_turtle = turtle.Turtle()
+
+def square():
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+
+square()
+qazi_turtle.forward(200)
+square()
---
+++ variables/python.py
@@ -0,0 +1,20 @@
+import turtle
+
+a = 5
+print(a)
+
+b = "hello"
+print(b)
+
+credit_card = 44443333222211111
+
+print(credit_card)
+
+qazi_turtle = turtle.Turtle()
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
