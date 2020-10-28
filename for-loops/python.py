b92fee45a06e2db6dfcb79ff28f5d98a19ed72bd
Widikin <widikin@Julies-MacBook-Pro.local>
2020-10-28 11:15:42 +0200

    learned about for and while loops

---
+++ for-loops/python.py
@@ -0,0 +1,46 @@
+import turtle
+
+a = 5
+print(a)
+
+b = "hello"
+print(b)
+
+credit_card = 44443333222211111
+
+print(credit_card)
+
+# Square
+
+qazi_turtle = turtle.Turtle()
+qazi_turtle.speed(15)
+
+def square():
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+
+#square()
+#qazi_turtle.forward(200)
+#square()
+
+elephant_weight = 3000
+ant_weight = 0.1
+
+#if elephant_weight > ant_weight:
+  # square()
+#else:
+    #qazi_turtle.forward(100)
+
+
+
+# qazi = 'happy'
+# while qazi == 'sad':
+ #   qazi_turtle.forward(10)
+
+for count in range(4):
+    square()
