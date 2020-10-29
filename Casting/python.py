d3ac15140f07637e8a2bc16c2656f96fd2f98438
Widikin <widikin@Julies-MacBook-Pro.local>
2020-10-29 11:22:16 +0200

    learned about data-types and casting

---
+++ Casting/python.py
@@ -0,0 +1,17 @@
+x = 1
+
+print(float(1))
+
+print(str(1))
+
+print(int("34"))
+
+print(5) # turns it it to a string before in displays in the console
+
+print(bool(0))
+
+print(bool(1))
+
+print(bool(-1))
+
+
---
+++ data-types/python.py
@@ -0,0 +1,13 @@
+
+# Integers / Numbers
+1 2 3 4 5
+
+# Floats / Decimal Numbers
+2.5, -0.3778
+
+# Strings / ''
+"hello how are you", 'I am fine and you'
+
+# Boolens / true or false
+True
+False
