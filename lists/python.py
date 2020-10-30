9ac711977ad45d9d590e40a77a8a54c10137655c
Widikin <widikin@Julies-MacBook-Pro.local>
2020-10-30 10:37:04 +0200

    learned about lists and strings

---
+++ lists/python.py
@@ -0,0 +1,13 @@
+# data structure
+
+numbers = [6, 4, 2, 9, 12]
+
+print(numbers)
+
+numbers.sort()
+
+print(numbers)
+
+
+for number in numbers:
+    print(number)
---
+++ strings/python.py
@@ -0,0 +1,3 @@
+s = "hello maam"
+
+print(s)
