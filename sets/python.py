4e4f50915dfc3cf061b40da0892a2fa3c72a32e0
Widikin <widikin@Julies-MacBook-Pro.local>
2020-11-01 11:30:27 +0200

    learned about tuples and sets

---
+++ sets/python.py
@@ -0,0 +1,10 @@
+# data type
+# union() joins the sets
+# intersection() displays you the dupicates
+# difference()
+library_1 = {'Harry Potter', 'Hunger Games', 'Lord of the Rings'}
+library_2 = {'Harry Potter', 'Romeo and Juliet'}
+
+diff = library_1.difference(library_2)
+
+print(diff)
---
+++ tuples/python.py
@@ -0,0 +1,17 @@
+# data type
+person1 = ("Nancy-pants", 25, 'Pizza')
+person2 = ("Joe-shirt", 20, 'Pasta')
+
+people = [person1, person2]
+
+# name, age, favfood = person
+
+# print(name)
+# print(age)
+# print(favfood)
+
+for name, age, favfood in people:
+    print(name)
+    print(age)
+    print(favfood)
+    print()
