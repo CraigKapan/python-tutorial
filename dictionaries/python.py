9a2de81237b365e2b8ca21e862d92db8144f4e2f
Widikin <widikin@Julies-MacBook-Pro.local>
2020-11-04 13:13:23 +0200

    learned about dictonaries

---
+++ dictionaries/python.py
@@ -0,0 +1,35 @@
+from collections import OrderedDict
+
+# data structure
+contacts = {
+    'Joe': {'phone': '123-4567', 'email': 'ea@website.com',
+    'Jane': {'987-6543', 'em@web.com'}
+}
+
+print(contacts['Jane'])
+
+
+sentence = 'I like the name Craig, because the name Craig is the best.'
+
+word_counts = {
+    'I': 1,
+    'like': 1,
+    'the': 3,
+}
+
+word_counts['Craig'] = 2
+
+# dict.items()
+
+# dict.keys()
+
+# dict.values()
+
+# dict.pop(key)
+
+# dict.popitem()
+
+# dict.popitem()
+
+# dict.clear()
+
